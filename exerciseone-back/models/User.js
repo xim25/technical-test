a8ae6e0c162fad92018f754a331539c1eb3e8609
Ximena Perez <ximxim25@gmail.com>
2019-01-22 22:38:44 -0600

    First Commit, models done

---
+++ exerciseone-back/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const  userSchema = new Schema ({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: String,
+  active: {
+    type: Number,
+    default: 1
+  },
+}, {
+  timestamps: {
+    createdAt: "creared_at",
+    updatedAt: "updated_at"
+  }
+});
+
+userSchema.plugin(passportLocalMongoose, {usernameField: "username"});
+
+module.exports = mongoose.model("User", userSchema);
