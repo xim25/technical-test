50ea2c10fe892deefb7ad65284197f6101e98051
Ximena Perez <ximxim25@gmail.com>
2019-01-24 00:41:28 -0600

    Test on Postman

--- exerciseone-back/app.js
+++ exerciseone-back/app.js
@@ -3,15 +3,14 @@ require('dotenv').config();
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const cors         = require('cors');
 
 
 mongoose
-  .connect('mongodb://localhost/exerciseone-back', {useNewUrlParser: true})
+  .connect(process.env.DB, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -30,6 +29,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+// CORS
+const corsOptions = {
+  origin: true,
+  credentials: true
+};
+app.use(cors(corsOptions));
+
+
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
@@ -42,7 +50,6 @@ app.use(require('node-sass-middleware')({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 
@@ -51,8 +58,18 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 
-const index = require('./routes/index');
-app.use('/', index);
+const auth = require('./routes/auth');
+const product = require('./routes/product');
+
+
+
+app.use('/api/auth', auth);
+app.use('/api/product', product);
+
+
+// app.all('/*', (req, res) => {
+//     res.sendFile(__dirname + '/public/index.html');
+// });
 
 
 module.exports = app;
--- exerciseone-back/models/Product.js
+++ exerciseone-back/models/Product.js
@@ -2,18 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema ({
-  name: {
-    type: String,
-    required: true
-  },
-  image: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  }
+
+  name: String,
+
+  image: String,
+
+  price: Number
+  
 });
 
 module.exports = mongoose.model("Product", productSchema);
